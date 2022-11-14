a51fd71f18751c3904b970817fadf06901f946e4
Krins <krinskumar@gmail.com>
2022-11-13 21:04:22 -0500

    a4-complete

--- a4/src/js/products.js
+++ a4/src/js/products.js
@@ -14,7 +14,7 @@
 
 window.products = [
   /*
-    {
+    { template
       id: "P1",
       name: "Name 1",
       description: "Description 1...",
@@ -61,7 +61,7 @@ window.products = [
 
   {
     id: `005`,
-    name: `Tezarre TK61Pro`,
+    name: `Tezarre TK61Pro Season`,
     description: `16.8 million color combinations of RGB modes including Wave, Twinkle, Aurora, Musical Rhythm, Radar and Ripple`,
     price: 5085,
     discontinued: false,
@@ -88,91 +88,178 @@ window.products = [
 
   {
     id: `008`,
-    name: ``,
-    description: ``,
-    price: 1,
+    name: `Logitexh MX `,
+    description: `Spherically dished keys match the shape of your fingertips, offering satisfying feedback with every tap`,
+    price: 14900,
     discontinued: false,
     categories: [
       "all-keyboards",
       "full-keyboards",
-      "mechanical-keyboards",
-      "optical-keyboards",
-      "compact-keyboards"
     ]
   },
 
   {
     id: `009`,
-    name: ``,
-    description: ``,
-    price: 1,
+    name: `Logitech ERGO k8`,
+    description: `Type more naturally with a curved, split KeyFrame. The sloping keyboard reduces strain on your wrists and forearms.`,
+    price: 12999,
     discontinued: false,
     categories: [
       "all-keyboards",
-      "full-keyboards",
       "mechanical-keyboards",
-      "optical-keyboards",
-      "compact-keyboards"
     ]
   },
 
   {
     id: `010`,
-    name: ``,
-    description: ``,
-    price: 1,
+    name: `MSI Vigor `,
+    description: `Brushed metal top plate and octagonal-shaped keycaps`,
+    price: 8299,
     discontinued: false,
     categories: [
       "all-keyboards",
-      "full-keyboards",
       "mechanical-keyboards",
+      "compact-keyboards"
+    ]
+  },
+
+  {
+    id: `011`,
+    name: `Logitexh G213`,
+    description: `A million combinations of lights`,
+    price: 5259,
+    discontinued: true,
+    categories: [
+      "all-keyboards",
+      "compact-keyboards"
+    ]
+  },
+
+  {
+    id: `012`,
+    name: `StellSeries Apex`,
+    description: `World's fastest keyboard`,
+    price: 29999,
+    discontinued: true,
+    categories: [
+      "all-keyboards",
       "optical-keyboards",
       "compact-keyboards"
     ]
   },
 
   {
-    id: `002`,
-    name: ``,
-    description: ``,
-    price: 1,
+    id: `013`,
+    name: `Hp Omen`,
+    description: `Features durable mechanical switches available in Blue (clicky), Brown (tactile) and Red (smooth linear) to meet your typing preference.`,
+    price: 19980,
     discontinued: false,
     categories: [
       "all-keyboards",
-      "full-keyboards",
-      "mechanical-keyboards",
       "optical-keyboards",
       "compact-keyboards"
     ]
   },
 
   {
-    id: `002`,
-    name: ``,
-    description: ``,
-    price: 1,
+    id: `014`,
+    name: `Keychron K6`,
+    description: `Laser Engraved Keycaps shine through ABS keycaps provide crisp legible legends in fully lit and dimly lit settings.`,
+    price: 19099,
     discontinued: false,
     categories: [
       "all-keyboards",
-      "full-keyboards",
-      "mechanical-keyboards",
       "optical-keyboards",
       "compact-keyboards"
     ]
   },
 
   {
-    id: `002`,
-    name: ``,
-    description: ``,
-    price: 1,
+    id: `015`,
+    name: `MX Mater 32`,
+    description: `Exclusive ROG switches feature centralized RGB lighting for all-round in-key illumination.`,
+    price: 4900,
+    discontinued: true,
+    categories: [
+      "all-keyboards",
+      "compact-keyboards"
+    ]
+  },
+
+  {
+    id: `0016`,
+    name: `Asus ROG`,
+    description: `Use Bluetooth to connect and switch between three devices at a time, enjoy up to 76 hours of low-latency gameplay in 2.4 GHz RF wireless mode, or use standard wired USB`,
+    price: 22294,
     discontinued: false,
     categories: [
       "all-keyboards",
       "full-keyboards",
-      "mechanical-keyboards",
       "optical-keyboards",
-      "compact-keyboards"
+    ]
+  },
+
+  {
+    id: `017`,
+    name: `Glorious GMMK`,
+    description: `As the first Hot Swap RGB gaming keyboard, GMMK features hot swap switch sockets so you can plug in and out switches as your DIY preference.`,
+    price: 3200,
+    discontinued: false,
+    categories: [
+      "all-keyboards",
+      "optical-keyboards",
+    ]
+  },
+
+  {
+    id: `018`,
+    name: `Seneca Series S`,
+    description: `Superior Ergonomics and Compatibility: With the in built stand, angle of the keyboard can be changed to match a more ergonomic typing experience.`,
+    price: 1000,
+    discontinued: true,
+    categories: [
+      "all-keyboards",
+      "mechanical-keyboards",
+    ]
+  },
+
+  {
+    id: `019`,
+    name: `Seneca Major`,
+    description: `kit comes with OEM sculpted keycaps so your fingers and hand joints can preserve the jelly like substance and do not become stiff over time.`,
+    price: 1800000000000000000,
+    discontinued: true,
+    categories: [
+      "all-keyboards",
+      "optical-keyboards",
+    ]
+  },
+
+  {
+    id: `020`,
+    name: `Bluff z-77`,
+    description: `60% mechanical keyboard adopts advanced metal panel design, beautiful and strong.`,
+    price: 18000,
+    discontinued: false,
+    categories: [
+      "all-keyboards",
+      "optical-keyboards",
+      "mechanical-keyboards",
+      "full-keyboards",
+    ]
+  },
+
+  {
+    id: `021`,
+    name: `AlienHouse Green Saber`,
+    description: `red switches mechanical keyboard adopted with classic red switches, that responds fast with no lag.`,
+    price: 8400,
+    discontinued: false,
+    categories: [
+      "all-keyboards",
+      "mechanical-keyboards",
+      "compact-keyboards",
+      "full-keyboards"
     ]
   }
 ];
